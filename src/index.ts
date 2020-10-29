fdc6e4a1b479d26d234ebdfd5112426e47687b56
Vlad Shilov <omgovich@ya.ru>
2020-10-29 19:01:51 +0300

    Initialize package and tooling

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Data
+export { categories } from "./similar-npm-packages";
+
+// Export types to help developers use our data
+export { Categories, Category, Tag, Weight } from "./internals/types";
